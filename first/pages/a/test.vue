86ec667c7befc4c58bea89d0f33f8dd10f963906
Klaiveft <pierre.bourgery@epitech.eu>
2023-12-09 18:17:26 +0100

    first commit

---
+++ first/pages/a/test.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Test</h1>
+    <p>test</p>
+  </div>
+</template>
---
+++ first/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+const message = "Hello Vue 3!";
+</script>
+
+<template>
+  <div>
+    <h1>My App</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
